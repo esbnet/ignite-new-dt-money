50983a2fe041eadb176f517a2130fc3e49bca636
Edmilson Soares <esbnet@gmail.com>
2022-11-10 12:51:31 -0300

    feat: :sparkles: create modal new transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,24 @@
+import * as Dialog from "@radix-ui/react-dialog"
+
 import { HeaderContainer, HeaderContent, NewTranslationButton } from "./styles"
 
 import Logo from "../../assets/logo.svg"
+import { NewTransactionModal } from "../NewTransactionModal"
 
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={Logo} alt='' />
-        <NewTranslationButton>Nova transação</NewTranslationButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTranslationButton>Nova transação</NewTranslationButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+          
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
