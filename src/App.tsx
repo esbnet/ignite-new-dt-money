921c6b54aa87870b677b47394e1f9f03d8183e8c
Edmilson Soares <esbnet@gmail.com>
2022-11-10 10:05:28 -0300

    feat: :sparkles: create cards and table to transaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from "styled-components"
-import { Header } from "./components/Header"
+import { Transactions } from "./pages/Transactions"
 import { GlobalStyle } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
@@ -7,7 +7,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <Header/>
+      <Transactions/>
     </ThemeProvider>
   )
 }
