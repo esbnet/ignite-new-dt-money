0f56039b5fb706807bf432f9a961e49a17aefbcf
Edmilson Soares <esbnet@gmail.com>
2022-11-11 18:44:49 -0300

    feat: :sparkles: create summary values to home

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,11 +1,28 @@
- import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react"
+import { access } from "fs"
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react"
 import { useContext } from "react"
 import { TrasactionsContext } from "../../contexts/TransactionsContext"
 import { SummaryCard, SummaryContainer } from "./styles"
 
 export function Summary() {
+  const { transactions } = useContext(TrasactionsContext)
 
-  const { transactions} = useContext(TrasactionsContext)
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price
+      } else {
+        acc.outcome += transaction.price
+      }
+      acc.total = acc.income - acc.outcome
+      return acc
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  )
 
   return (
     <SummaryContainer>
@@ -15,7 +32,7 @@ export function Summary() {
           <ArrowCircleUp size={32} color='#00b37e' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{"R$ "+ summary.income}</strong>
       </SummaryCard>
       <SummaryCard>
         <header>
@@ -23,15 +40,15 @@ export function Summary() {
           <ArrowCircleDown size={32} color='#f75a68' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{"R$ "+ summary.outcome}</strong>
       </SummaryCard>
-      <SummaryCard variant="green">
+      <SummaryCard variant='green'>
         <header>
           <span>Total</span>
           <CurrencyDollar size={32} color='#fff' />
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{"R$ "+ summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
